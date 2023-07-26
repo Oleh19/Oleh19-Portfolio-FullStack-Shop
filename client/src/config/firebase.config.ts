312ca672524cb2a955c455be04c0c512f3b62c6e
Oleh19 <ilchenko19061998@gmail.com>
2023-07-26 17:19:07 +0300

    .

--- client/src/config/firebase.config.ts
+++ client/src/config/firebase.config.ts
@@ -1,13 +1,14 @@
 import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
+import { REACT_APP_FIREBASE_API_KEY, REACT_APP_FIREBASE_AUTH_DOMAIN,REACT_APP_FIREBASE_PROJECT_ID, REACT_APP_FIREBASE_STORAGE_BUCCKET, REACT_APP_FIREBASE_MESSEAGING_ID, REACT_APP_FIREBASE_APP_ID } from './constants';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAkg91kWIqFh_iKUlbGCB8m-G9SJLV-XXU',
-  authDomain:'foodshop-2933e.firebaseapp.com',
-  projectId: 'foodshop-2933e',
-  storageBucket: 'foodshop-2933e.appspot.com',
-  messagingSenderId: '639680139462',
-  appId:'1:639680139462:web:038e5cc819f98b417d07c7'
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain:REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSEAGING_ID,
+  appId:REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
